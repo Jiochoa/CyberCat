ac8df88191d3e5b5e5ce8fa8d41bdf0815353d8b
Juan Ochoa <jochoa105@gmail.com>
2021-12-06 21:10:40 -0700

    Tiled Editor practice

---
+++ CyberCat_v5/Assets/Tiled Tutorial/Background.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="Background" tilewidth="32" tileheight="32" spacing="6" tilecount="16" columns="4">
+ <image source="Sample tiles/Background.png" width="148" height="148"/>
+</tileset>
---
+++ CyberCat_v5/Assets/Tiled Tutorial/Ground.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="Ground" tilewidth="32" tileheight="32" spacing="6" tilecount="30" columns="5">
+ <image source="Sample tiles/Ground.png" width="184" height="222"/>
+</tileset>
