e7c44ea553e09405b5179f471a3edc936ed30ee4
Juan Ochoa <jochoa105@gmail.com>
2021-12-14 20:23:17 -0700

    added default background, need adjustments

---
+++ CyberCat_v5/Assets/SuperTiled2Unity/Tiled Level Exports/Actual_Tileset.tsx
@@ -0,0 +1,93 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="Actual_Tileset" tilewidth="16" tileheight="16" tilecount="256" columns="16">
+ <image source="Tiles/Actual_Tileset.png" width="256" height="256"/>
+ <tile id="17">
+  <objectgroup draworder="index" id="3">
+   <object id="2" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="19">
+  <objectgroup draworder="index" id="2">
+   <object id="4" x="15.8261" y="0.173913">
+    <polygon points="0,0 0.0869565,15.7826 -15.6957,15.8261"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="20">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.0434783" y="0.173913">
+    <polygon points="0,0 15.913,15.6522 0,15.6957"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="22">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.173913" y="15.9565">
+    <polygon points="0,0 15.7826,-8.04348 15.7826,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="23">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="16.0435" y="0.130435">
+    <polygon points="0,0 -16.087,7.65217 -16,15.8261 0,15.7826"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="24">
+  <objectgroup draworder="index" id="2">
+   <object id="2" x="0.217391" y="0.0869565">
+    <polygon points="0,0 15.6957,7.65217 15.6957,15.9565 -0.0869565,15.913"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="25">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.173913" y="8.04348">
+    <polygon points="0,0 15.7391,7.95652 -0.130435,7.95652"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="35">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.0869565" y="0.0434783">
+    <polygon points="0,0 15.8696,0 15.8261,15.9565"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="36">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="15.8696" y="0.217391">
+    <polygon points="0,0 -15.7391,15.7826 -15.8261,-0.0434783"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="38">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.0869565" y="0.0434783">
+    <polygon points="0,0 15.8696,8.08696 15.913,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="39">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="15.9565" y="16">
+    <polygon points="0,0 -15.8696,-7.82609 -15.9565,-15.9565 0.0869565,-16"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="40">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.173913" y="16">
+    <polygon points="0,0 15.7391,-7.86957 15.8261,-15.9565 -0.173913,-15.9565"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="41">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="15.9565" y="0.0869565">
+    <polygon points="0,0 -15.7826,8.04348 -15.8696,0.0869565"/>
+   </object>
+  </objectgroup>
+ </tile>
+</tileset>
